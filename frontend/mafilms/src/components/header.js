1dc8817de6b251007805e411ff3691b544d92861
melih-avci <melih-avci@outlook.de>
2023-08-05 23:16:49 +0200

    header changes

--- frontend/mafilms/src/components/header.js
+++ frontend/mafilms/src/components/header.js
@@ -3,7 +3,7 @@ import Logo from "./logo";
 
 const Header = () => {
   return (
-    <div>
+    <div className="fixed top-0 left-0 w-full bg-white dark:bg-gray-900 py-4 px-8 flex items-center justify-between">
       <Logo />
       <h1 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
         <Link href="/" className="hover:underline">
