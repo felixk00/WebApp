3ee0cf5799c96dbf8fd9a8fc2f3ea5499e8dc8dd
melih-avci <melih-avci@outlook.de>
2023-08-06 11:30:12 +0200

    adjusted logo glow

--- frontend/mafilms/src/components/logo.js
+++ frontend/mafilms/src/components/logo.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "../styles/utils.module.css"; // Import the utils module CSS
 
 const Logo = () => {
   return (
     <Link href="/">
-      <div className="relative flex place-items-center before:absolute before:h-[150px] before:w-[240px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[90px] after:w-[120px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[180px] z-[-1]">
+      <div className={`relative flex place-items-center ${styles.glow}`}>
+        {/* Use styles.glow to apply the glow effect */}
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/assets/logos/MA_FILMS_Logo_3.1.svg"
