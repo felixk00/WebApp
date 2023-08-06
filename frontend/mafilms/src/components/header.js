86ed42d6c64d1fd976649029575a142beeddce38
melih-avci <melih-avci@outlook.de>
2023-08-06 14:01:31 +0200

    Header almost done

--- frontend/mafilms/src/components/header.js
+++ frontend/mafilms/src/components/header.js
@@ -3,17 +3,25 @@ import Logo from "./logo";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 w-full selection:py-3 px-6 flex">
-      <div
-        className="flex items-center justify-center w-full"
-        style={{ marginTop: "3%" }}
-      >
+    <header className="fixed top-0 left-0 w-full selection:py-3 px-6 grid grid-cols-3">
+      <nav className="col-start-1 flex items-center space-x-6">
+        <Link
+          className="text-gray-700 dark:text-gray-200 hover:underline"
+          href="/"
+        >
+          Showroom
+        </Link>
+      </nav>
+      <div className="col-start-2 flex items-center justify-center mt-8">
         <Logo />
       </div>
-      <nav
-        className="flex space-x-6"
-        style={{ marginTop: "3%", marginRight: "1%" }}
-      >
+      <nav className="col-start-3 flex items-center justify-end space-x-6">
+        <Link
+          className="text-gray-700 dark:text-gray-200 hover:underline"
+          href="/"
+        >
+          Contact
+        </Link>
         <Link
           className="text-gray-700 dark:text-gray-200 hover:underline"
           href="/"
