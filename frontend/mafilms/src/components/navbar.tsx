f4a64983fb1a60f171f5fbeecad749f5da74b993
melih-avci <melih-avci@outlook.de>
2023-08-21 21:30:33 +0200

    Added responsive navbar

--- frontend/mafilms/src/components/navbar.tsx
+++ frontend/mafilms/src/components/navbar.tsx
@@ -1,18 +1,83 @@
+"use client";
+
 import { mainLinks } from "../links/navbarLinks";
 import Link from "next/link";
 import Logo from "./logo";
-
-const { COMPANY_NAME, SITE_NAME } = process.env;
+import React, { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#ffffff");
+        setTextColor("#000000");
+      } else {
+        setColor("transparent");
+        setTextColor("#ffffff");
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
-    <header className="text-sm text-neutral-600 dark:text-neutral-300">
-      <div className="justify-between items-center mx-auto flex w-full flex-row gap-6 border-b border-neutral-400 px-6 py-8 dark:border-neutral-600">
-        <div className="flex flex-col items-center md:pl-10 pl-5">
-          <Logo width={65} height={20} glow={true} />
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
+        <Logo width={65} height={20} glow={true} />
+
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+          {mainLinks.map((link) => (
+            <li className="p-4" key={link.label}>
+              <Link className="hover:underline" href={link.href}>
+                {link.label}
+              </Link>
+            </li>
+          ))}
+        </ul>
+
+        {/* Mobile Button */}
+        <div onClick={handleNav} className="block sm:hidden z-10">
+          {nav ? (
+            <AiOutlineClose size={20} style={{ color: `${textColor}` }} />
+          ) : (
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
+          )}
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={
+            nav
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300"
+          }
+        >
+          <ul>
+            {mainLinks.map((link) => (
+              <li
+                onClick={handleNav}
+                className="p-4 text-4xl hover:text-gray-500"
+                key={link.label}
+              >
+                <Link className="hover:underline" href={link.href}>
+                  {link.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
-    </header>
+    </div>
   );
 };
 
