73295fa6ddb3ae145aa2c4c19aeb313b0d3f7969
melih-avci <melih-avci@outlook.de>
2023-08-27 11:49:01 +0200

    prettier tailwind plugin for reordering classes

--- frontend/mafilms/src/components/footer.tsx
+++ frontend/mafilms/src/components/footer.tsx
@@ -1,40 +1,40 @@
-import { Links, mainLinks } from "../links/links";
-import Link from "next/link";
-import Logo from "./logo";
+import { Links, mainLinks } from '../links/links';
+import Link from 'next/link';
+import Logo from './logo';
 
 const { COMPANY_NAME, SITE_NAME } = process.env;
 const Footer = () => {
   const currentYear = new Date().getFullYear();
-  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : "");
-  const copyrightName = COMPANY_NAME ?? SITE_NAME ?? "Melih Dihya";
+  const copyrightDate = 2023 + (currentYear > 2023 ? `-${currentYear}` : '');
+  const copyrightName = COMPANY_NAME ?? SITE_NAME ?? 'Melih Dihya';
 
   return (
-    <footer className="text-neutral-500 dark:text-neutral-400">
-      <div className="justify-between items-center mx-auto flex w-full max-w-7xl flex-col gap-6 border-t border-neutral-200 px-4 py-12 dark:border-neutral-700 sm:flex-row sm:gap-12">
-        <div className="flex flex-col items-center">
-          <Link href="/">
+    <footer className='text-neutral-500 dark:text-neutral-400'>
+      <div className='mx-auto flex w-full max-w-7xl flex-col items-center justify-between gap-6 border-t border-neutral-200 px-4 py-12 dark:border-neutral-700 sm:flex-row sm:gap-12'>
+        <div className='flex flex-col items-center'>
+          <Link href='/'>
             <Logo />
           </Link>
-          <h3 className="text-lg py-5">Capturing in a Place of No Mind</h3>
+          <h3 className='py-5 text-lg'>Capturing in a Place of No Mind</h3>
         </div>
 
-        <div className="flex flex-row space-x-10 md:text-lg">
-          <ul className="flex flex-col text-left tracking-tight md:tracking-tighter">
-            <h3 className="mb-2 font-bold ">MA Films</h3>
+        <div className='flex flex-row space-x-10 md:text-lg'>
+          <ul className='flex flex-col text-left tracking-tight md:tracking-tighter'>
+            <h3 className='mb-2 font-bold '>MA Films</h3>
             {mainLinks.map((link) => (
               <li key={link.label}>
-                <Link className="hover:underline" href={link.href}>
+                <Link className='hover:underline' href={link.href}>
                   {link.label}
                 </Link>
               </li>
             ))}
           </ul>
 
-          <ul className="flex flex-col text-left tracking-tight md:tracking-tighter">
-            <h3 className="mb-2 font-bold ">Links</h3>
+          <ul className='flex flex-col text-left tracking-tight md:tracking-tighter'>
+            <h3 className='mb-2 font-bold '>Links</h3>
             {Links.map((link) => (
               <li key={link.label}>
-                <Link className="hover:underline" href={link.href}>
+                <Link className='hover:underline' href={link.href}>
                   {link.label}
                 </Link>
               </li>
@@ -43,22 +43,22 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
-        <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 sm:flex-row sm:gap-0 px-4">
+      <div className='border-t border-neutral-200 py-6 text-sm dark:border-neutral-700'>
+        <div className='mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 sm:flex-row sm:gap-0'>
           <p>
             &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith(".")
-              ? "."
-              : ""}{" "}
+            {copyrightName.length && !copyrightName.endsWith('.')
+              ? '.'
+              : ''}{' '}
             All rights reserved.
           </p>
-          <hr className="mx-4 hidden h-4 w-[1px] border-l border-neutral-400 sm:inline-block" />
+          <hr className='mx-4 hidden h-4 w-[1px] border-l border-neutral-400 sm:inline-block' />
           <p>Designed in Karlsruhe</p>
-          <p className="sm:ml-auto">
-            Crafted by{" "}
+          <p className='sm:ml-auto'>
+            Crafted by{' '}
             <a
-              href="https://www.instagram.com/melihdihya_sw/"
-              className="text-black dark:text-white"
+              href='https://www.instagram.com/melihdihya_sw/'
+              className='text-black dark:text-white'
             >
               Melih Dihya
             </a>
--- frontend/mafilms/src/components/navbar.tsx
+++ frontend/mafilms/src/components/navbar.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client';
 
-import { mainLinks } from "../links/links";
-import Link from "next/link";
-import Logo from "./logo";
-import React, { useState, useEffect } from "react";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import styles from "../styles/utils.module.css";
-import { usePathname } from "next/navigation";
+import { mainLinks } from '../links/links';
+import Link from 'next/link';
+import Logo from './logo';
+import React, { useState, useEffect } from 'react';
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
+import styles from '../styles/utils.module.css';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
   const routeDisplayNames: Record<string, string> = {
-    "/": "Home",
-    "/showroom": "Showroom",
-    "/blog": "Blog",
-    "/about": "About",
-    "/contact": "Contact",
-    "/legalnotes/imprint": "Imprint",
-    "/legalnotes/privacy": "Privacy",
+    '/': 'Home',
+    '/showroom': 'Showroom',
+    '/blog': 'Blog',
+    '/about': 'About',
+    '/contact': 'Contact',
+    '/legalnotes/imprint': 'Imprint',
+    '/legalnotes/privacy': 'Privacy',
   };
 
   const [nav, setNav] = useState(false);
-  const [border, setBorder] = useState("");
+  const [border, setBorder] = useState('');
   const currentRoute = usePathname();
 
   const mobileMenuClassName = `${styles.colors} sm:hidden absolute top-0 left-0 right-0 bottom-0 flex flex-col justify-center items-center w-full h-screen text-center ease-in duration-300`;
@@ -34,43 +34,43 @@ const Navbar = () => {
   useEffect(() => {
     const applyBorder = () => {
       if (window.scrollY >= 50) {
-        setBorder("border-b border-neutral-400 dark:border-neutral-300");
+        setBorder('border-b border-neutral-400 dark:border-neutral-300');
       } else {
-        setBorder("");
+        setBorder('');
       }
     };
-    window.addEventListener("scroll", applyBorder);
+    window.addEventListener('scroll', applyBorder);
   }, []);
 
   useEffect(() => {
     if (nav) {
-      document.documentElement.style.overflow = "hidden";
+      document.documentElement.style.overflow = 'hidden';
     } else {
-      document.documentElement.style.overflow = "auto";
+      document.documentElement.style.overflow = 'auto';
     }
     return () => {
-      document.documentElement.style.overflow = "auto";
+      document.documentElement.style.overflow = 'auto';
     };
   }, [nav]);
 
   return (
     <div
-      className={`${border} ${styles.navbar} transition-all fixed left-0 top-0 w-full z-10 text-neutral-600 dark:text-neutral-300`}
+      className={`${border} ${styles.navbar} fixed left-0 top-0 z-10 w-full text-neutral-600 transition-all dark:text-neutral-300`}
     >
-      <div className="max-w-7xl m-auto flex justify-between items-center px-4 py-1">
-        <Link href="/">
+      <div className='m-auto flex max-w-7xl items-center justify-between px-4 py-1'>
+        <Link href='/'>
           <Logo />
         </Link>
         {/* Desktop Navigation */}
-        <ul className="hidden sm:flex">
+        <ul className='hidden sm:flex'>
           {mainLinks.map((link) => (
             <li
               className={`px-4 text-lg ${
-                currentRoute === link.href ? "text-red-700" : ""
+                currentRoute === link.href ? 'text-red-700' : ''
               }`}
               key={link.label}
             >
-              <Link className="hover:text-gray-500" href={link.href}>
+              <Link className='hover:text-gray-500' href={link.href}>
                 {link.label}
               </Link>
             </li>
@@ -78,13 +78,13 @@ const Navbar = () => {
         </ul>
 
         {currentRoute && (
-          <span className="block sm:hidden text-2xl text-red-700">
-            {routeDisplayNames[currentRoute] || "Unknown"}
+          <span className='block text-2xl text-red-700 sm:hidden'>
+            {routeDisplayNames[currentRoute] || 'Unknown'}
           </span>
         )}
 
         {/* Mobile Button */}
-        <div onClick={handleNav} className="block sm:hidden z-10">
+        <div onClick={handleNav} className='z-10 block sm:hidden'>
           {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30} />}
         </div>
 
@@ -94,19 +94,19 @@ const Navbar = () => {
             nav ? mobileMenuVisibleClassName : mobileMenuHiddenClassName
           }
         >
-          <Link href="/" className="mb-8" onClick={handleNav}>
+          <Link href='/' className='mb-8' onClick={handleNav}>
             <Logo />
           </Link>
-          <ul className="mb-14">
+          <ul className='mb-14'>
             {mainLinks.map((link) => (
               <li
                 onClick={handleNav}
                 className={`p-4 text-4xl ${
-                  currentRoute === link.href ? "text-red-700" : ""
+                  currentRoute === link.href ? 'text-red-700' : ''
                 }`}
                 key={link.label}
               >
-                <Link className="hover:text-gray-500" href={link.href}>
+                <Link className='hover:text-gray-500' href={link.href}>
                   {link.label}
                 </Link>
               </li>
