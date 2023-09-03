e3d7f774b929619b9752eff46284f17210b1eec8
melih-avci <melih-avci@outlook.de>
2023-09-03 22:58:24 +0200

    z value

--- frontend/mafilms/src/app/(main)/showroom/liguria/Client.tsx
+++ frontend/mafilms/src/app/(main)/showroom/liguria/Client.tsx
@@ -50,7 +50,7 @@ export default function Client({ images }: { images: ImageProps[] }) {
               Mountains and coasts full of charm.
             </p>
             <a
-              className='pointer z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4'
+              className='pointer z-0 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold text-black transition hover:bg-white/10 hover:text-white md:mt-4'
               href=''
               target='_blank'
               rel='noreferrer'
--- frontend/mafilms/src/components/Navbar.tsx
+++ frontend/mafilms/src/components/Navbar.tsx
@@ -43,7 +43,7 @@ const Navbar = () => {
 
   return (
     <div
-      className={`${border} ${styles.navbar} fixed left-0 top-0 z-20 w-full text-neutral-600 transition-all dark:text-neutral-300`}
+      className={`${border} ${styles.navbar} fixed left-0 top-0 z-10 w-full text-neutral-600 transition-all dark:text-neutral-300`}
     >
       <div className='m-auto flex max-w-7xl items-center justify-between px-4 py-1'>
         <Link href='/'>
