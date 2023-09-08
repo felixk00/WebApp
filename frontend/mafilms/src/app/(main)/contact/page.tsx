d52b51b8fd616147a0f7ec40bb45599324a4e72c
felixk00 <kienlefe@hs-pforzheim.de>
2023-09-08 23:15:45 +0200

    some explanatory text for the form

--- frontend/mafilms/src/app/(main)/contact/page.tsx
+++ frontend/mafilms/src/app/(main)/contact/page.tsx
@@ -11,7 +11,12 @@ export default function Contact() {
 
   return (
     <div className="mx-auto max-w-2xl px-4 my-10">
-      <h1>Contact us</h1>
+      <h1 className="text-4xl font-bold leading-relaxed">Contact us</h1>
+      
+      <p className="text-1xl">If you want to get in touch just fill
+      out this form and we will look in to it. Give us <b>some details</b> about your request
+      in the <b>message field.</b> We are happy to answer your questions.</p>
+     
 
       <ContactForm />
     </div>
