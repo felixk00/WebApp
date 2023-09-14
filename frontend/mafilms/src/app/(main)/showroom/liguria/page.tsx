f0a52822a6b153f67e88de724055aa28fd9fc0bd
melih-avci <melih-avci@outlook.de>
2023-09-14 07:50:32 +0200

    switched to route handler to fetch images from cdn

--- frontend/mafilms/src/app/(main)/showroom/liguria/page.tsx
+++ frontend/mafilms/src/app/(main)/showroom/liguria/page.tsx
@@ -1,4 +1,3 @@
-import { FetchImages } from '../../../../utils/FetchImages';
 import Client from './Client';
 
 export const metadata = {
@@ -7,7 +6,7 @@ export const metadata = {
 };
 
 export default async function Page() {
-  const images = await FetchImages();
-
+  const response = await fetch('http://localhost:3000/api/cdn');
+  const images = await response.json();
   return <Client images={images} />;
 }
---
+++ frontend/mafilms/src/app/api/cdn/route.ts
@@ -0,0 +1,40 @@
+import cloudinary from '@/utils/cloudinary';
+import type { ImageProps } from '../../../utils/types';
+import getBase64ImageUrl from '../../../utils/generateBlurPlaceholder';
+import { NextResponse } from 'next/server';
+
+export async function GET() {
+  try {
+    const results = await cloudinary.v2.search
+      .expression(`folder:${process.env.CLOUDINARY_FOLDER}/*`)
+      .sort_by('public_id', 'desc')
+      .max_results(400)
+      .execute();
+    let reducedResults: ImageProps[] = [];
+
+    let i = 0;
+    for (let result of results.resources) {
+      reducedResults.push({
+        id: i,
+        height: result.height,
+        width: result.width,
+        public_id: result.public_id,
+        format: result.format,
+      });
+      i++;
+    }
+
+    const blurImagePromises = results.resources.map((image: ImageProps) => {
+      return getBase64ImageUrl(image);
+    });
+    const imagesWithBlurDataUrls = await Promise.all(blurImagePromises);
+
+    for (let i = 0; i < reducedResults.length; i++) {
+      reducedResults[i].blurDataUrl = imagesWithBlurDataUrls[i];
+    }
+    return NextResponse.json(reducedResults);
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    return NextResponse.error();
+  }
+}
