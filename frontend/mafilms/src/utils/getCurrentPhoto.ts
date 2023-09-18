719943ccfaf03aabb8139d621a9b884484f53619
melih-avci <melih-avci@outlook.de>
2023-09-19 00:09:27 +0200

    Refactor

--- frontend/mafilms/src/utils/getCurrentPhoto.ts
+++ frontend/mafilms/src/utils/getCurrentPhoto.ts
@@ -2,8 +2,8 @@ import getResults from './cachedImages';
 import getBase64ImageUrl from './generateBlurPlaceholder';
 import type { ImageProps } from './types';
 
-export const getCurrentPhoto = async (photoId: string): Promise<ImageProps> => {
-  const results = await getResults();
+export const getCurrentPhoto = async (photoId: string, folder: string): Promise<ImageProps> => {
+  const results = await getResults(folder);
 
   let reducedResults: ImageProps[] = [];
   let i = 0;
