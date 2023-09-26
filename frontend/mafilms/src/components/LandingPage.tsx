6ac6bc00d0c40296e4aa5374e60f541f834dea8b
felixk00 <kienlefe@hs-pforzheim.de>
2023-09-26 22:42:41 +0200

    added landing page content

---
+++ frontend/mafilms/src/components/LandingPage.tsx
@@ -0,0 +1,24 @@
+
+
+export default function LandingPage() {
+    return (
+        <div className="mx-auto flex max-w-6xl flex-col justify-between relative">
+
+<h1 className="absolute left-7 top-7 text-7xl text-white font-bold">Capture</h1>
+
+
+
+            <img
+                src="/assets/pictures/DSC_0305.jpg"
+                alt="TEstfoto"
+                className="w-full h-auto rounded-xl"
+
+            />
+            <a id="lpageButton" className="absolute bottom-7 left-7 text-2xl text-center font-bold px-4 py-2 w-64 border border-white bg-white
+            transition hover:bg-white/10 hover:text-white rounded-xl" href="/showroom/">Zum Portfolio</a>
+        
+
+
+</div>
+    );
+} 
