e0d29f01a36c6d6a97f3fe62038836105ecbed79
felixk00 <kienlefe@hs-pforzheim.de>
2023-09-30 16:14:22 +0200

    animation, title pic and button is now responsive

--- frontend/mafilms/src/components/LandingPage.tsx
+++ frontend/mafilms/src/components/LandingPage.tsx
@@ -2,12 +2,13 @@
 
 export default function LandingPage() {
     return (
-        <div className="mx-auto flex max-w-7xl flex-col justify-between relative">
+        <div className='mx-auto flex items-center justify-center p-4'>
+            <div className="mx-auto flex max-w-4xl flex-col justify-between relative">
 
-            <div className="mx-auto flex max-w-6xl flex-col justify-between relative">
 
 
-                <div className="absolute left-7 top-7 text-7xl text-white font-bold">
+
+                <div className="absolute lg:px-6 lg:py-4 px-4 py-2 lg:text-7xl text-2xl text-white font-bold">
                     <div className="animate-typing1">
                         Capture Moments
                     </div>
@@ -15,6 +16,8 @@ export default function LandingPage() {
                         like never before.
                     </div>
                 </div>
+
+              
 
 
 
@@ -25,46 +28,33 @@ export default function LandingPage() {
                     className="w-full h-auto rounded-lg"
 
                 />
-                <a id="lpageButton" className="absolute bottom-7 left-7 text-2xl text-center font-bold px-4 py-2 w-64 border border-white bg-white
-            transition hover:bg-white/10 hover:text-white rounded-lg" href="/showroom/">Zum Portfolio</a>
+                {/* Desktop button */}
+                <a
+                    id="desktopButton"
+                    className="absolute bottom-7 left-7 hidden md:inline-block text-center px-4 py-2 text-lg font-bold w-48 border border-white bg-white transition hover:bg-white/20 hover:text-white rounded-lg dark:text-black"
+                    href="/showroom/"
+                >
+                    Zum Portfolio
+                </a>
+
+                {/* Mobile and tablet button */}
+                <a
+                    id="mobileButton"
+                    className="absolute bottom-4 left-4 md:hidden text-center px-4 py-2 font-bold w-36 border border-white bg-white transition hover:bg-white/10 hover:text-white rounded-lg dark:text-black"
+                    href="/showroom/"
+                >
+                    Zum Portfolio
+                </a>
+
 
 
 
             </div>
-            <br />
-            <br />
-            <div className="mx-auto flex max-w-6xl flex-col justify-between relative">
 
 
-                <h1 className="text-3xl font-bold leading-relaxed">Welcome to MA-Films!</h1>
 
-                <p className="text-2xl">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                    aliquyam erat, sed diam voluptua. At vero eos et accusam et
-                    justo duo dolores et ea rebum. Stet clita kasd gubergren, no
-                    sea takimata sanctus est Lorem ipsum dolor sit amet.
-                </p>
-
-
-            </div>
-
-            <br />
-            <br />
-            <div className="mx-auto flex max-w-6xl flex-col justify-between relative">
-
-                <img
-                    src="/assets/pictures/DSC_0305.jpg"
-                    alt="TEstfoto"
-                    className="w-1/2 left-0 h-auto rounded-lg"
-
-                />
-             
-                
-            </div>
 
         </div>
-
 
     );
 } 
