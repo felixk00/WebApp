ccaec0bc86598e6f692a71ee25b902c8f19054ea
felixk00 <kienlefe@hs-pforzheim.de>
2023-10-02 21:27:05 +0200

    made imprint responsive

--- frontend/mafilms/src/components/ImprintComponent.tsx
+++ frontend/mafilms/src/components/ImprintComponent.tsx
@@ -2,83 +2,84 @@
 export default function ImprintComponent() {
 
     return (
+        <div className='mx-auto flex items-center justify-center p-4'>
+            <div className="mx-auto flex max-w-7xl flex-col justify-between">
+                <h1 className="text-5xl font-bold leading-relaxed">Imprint</h1>
+                <br />
+                <h1 className="text-2xl underline font-bold leading-relaxed">Information according to § 5 TMG:</h1>
+                <p className="text-lg">Melih Dihya</p>
+                <p className="text-lg">Felix Kienle</p>
+                <p className="text-lg">Hauptstr. 5</p>
+                <p className="text-lg">76139 Karlsruhe</p>
+                <br></br>
+                <h1 className="text-2xl underline font-bold leading-relaxed">Contact</h1>
+                <p className="text-lg">
+                    Tel. 01237654654
+                </p>
+                <p className="text-lg">
+                    E-Mail: info@mafilms.com
+                </p>
+                <br />
+                <h1 className="text-2xl font-bold leading-relaxed">Disclaimer</h1>
+                <h1 className="text-2xl underline leading-relaxed">Liability for content</h1>
+                <p className="text-lg">
+                    All content on our website was created with the greatest care and to the best of
+                    our knowledge. However, we cannot guarantee that the content is correct, complete or
+                    up-to-date. As a service provider, we are responsible for our own content on these
+                    pages in accordance with general laws in accordance with Section 7 Paragraph 1 TMG. However,
+                    according to Sections 8 to 10 TMG, as a service provider we are not obliged to monitor
+                    transmitted or stored third-party information or to investigate circumstances that
+                    indicate illegal activity. Obligations to remove or block the use of information in
+                    accordance with general law remain unaffected.
+                </p>
+                <p className="text-lg">However, liability in this regard is only possible from the time
+                    we become aware of a specific legal violation. If we become aware of the above-mentioned
+                    legal violations, we will immediately remove this content.
+                </p>
+                <h1 className="text-2xl underline leading-relaxed">Limitation of liability for external links</h1>
 
-        <div className="mx-auto flex max-w-7xl flex-col justify-between">
-            <h1 className="text-5xl font-bold leading-relaxed">Imprint</h1>
-            <br />
-            <h1 className="text-2xl underline font-bold leading-relaxed">Information according to § 5 TMG:</h1>
-            <p className="text-lg">Melih Dihya</p>
-            <p className="text-lg">Felix Kienle</p>
-            <p className="text-lg">Hauptstr. 5</p>
-            <p className="text-lg">76139 Karlsruhe</p>
-            <br></br>
-            <h1 className="text-2xl underline font-bold leading-relaxed">Contact</h1>
-            <p className="text-lg">
-                Tel. 01237654654
-            </p>
-            <p className="text-lg">
-                E-Mail: info@mafilms.com
-            </p>
-            <br />
-            <h1 className="text-2xl font-bold leading-relaxed">Disclaimer</h1>
-            <h1 className="text-2xl underline leading-relaxed">Liability for content</h1>
-            <p className="text-lg">
-                All content on our website was created with the greatest care and to the best of
-                our knowledge. However, we cannot guarantee that the content is correct, complete or
-                up-to-date. As a service provider, we are responsible for our own content on these
-                pages in accordance with general laws in accordance with Section 7 Paragraph 1 TMG. However,
-                according to Sections 8 to 10 TMG, as a service provider we are not obliged to monitor
-                transmitted or stored third-party information or to investigate circumstances that
-                indicate illegal activity. Obligations to remove or block the use of information in
-                accordance with general law remain unaffected.
-            </p>
-            <p className="text-lg">However, liability in this regard is only possible from the time
-                we become aware of a specific legal violation. If we become aware of the above-mentioned
-                legal violations, we will immediately remove this content.
-            </p>
-            <h1 className="text-2xl underline leading-relaxed">Limitation of liability for external links</h1>
+                <p className="text-lg">
+                    Our website contains links to external third-party websites. We have no influence
+                    on the content of these directly or indirectly linked websites. We therefore cannot
+                    assume any liability for the accuracy of the content of “external links”. The respective
+                    providers or operators (authors) of the pages are responsible for the content of the
+                    external links.
+                </p>
+                <p className="text-lg">
+                    The external links were checked for possible legal violations at the time the link was
+                    created and were free of illegal content at the time the link was created. A constant
+                    check of the content of external links is not possible without concrete evidence of a
+                    legal violation. In the case of direct or indirect links to third-party websites that
+                    are outside our area of ​​responsibility, liability would only exist if we became aware
+                    of the content and it would be technically possible and reasonable for us to prevent
+                    use in the event of illegal content .
+                </p>
+                <p className="text-lg">This disclaimer of liability also applies to links and references
+                    from questioners, blog entries and guests of the discussion forum within the “name of
+                    your domain” website. The service provider of the page to which reference is made is
+                    solely liable for illegal, incorrect or incomplete content and in particular for damage
+                    resulting from the use or non-use of such information, not the person who merely refers
+                    to the respective publication via links.
+                </p>
+                <p className="text-lg">If we become aware of legal violations, we will immediately remove
+                    the external links.</p>
+                <h1 className="text-2xl underline leading-relaxed">Liability for content</h1>
 
-            <p className="text-lg">
-                Our website contains links to external third-party websites. We have no influence
-                on the content of these directly or indirectly linked websites. We therefore cannot
-                assume any liability for the accuracy of the content of “external links”. The respective
-                providers or operators (authors) of the pages are responsible for the content of the
-                external links.
-            </p>
-            <p className="text-lg">
-                The external links were checked for possible legal violations at the time the link was
-                created and were free of illegal content at the time the link was created. A constant
-                check of the content of external links is not possible without concrete evidence of a
-                legal violation. In the case of direct or indirect links to third-party websites that
-                are outside our area of ​​responsibility, liability would only exist if we became aware
-                of the content and it would be technically possible and reasonable for us to prevent
-                use in the event of illegal content .
-            </p>
-            <p className="text-lg">This disclaimer of liability also applies to links and references
-                from questioners, blog entries and guests of the discussion forum within the “name of
-                your domain” website. The service provider of the page to which reference is made is
-                solely liable for illegal, incorrect or incomplete content and in particular for damage
-                resulting from the use or non-use of such information, not the person who merely refers
-                to the respective publication via links.
-            </p>
-            <p className="text-lg">If we become aware of legal violations, we will immediately remove
-                the external links.</p>
-            <h1 className="text-2xl underline leading-relaxed">Liability for content</h1>
-
-            <p className="text-lg">The content and works published on our website are subject to German 
-            copyright law (http://www.gesetze-im-internet.de/bundesrecht/urhg/kritik.pdf). The reproduction, 
-            processing, distribution and any kind of exploitation of the intellectual property from an 
-            intellectual and material perspective of the author outside the limits of copyright law requires 
-            the prior written consent of the respective author in accordance with the Copyright 
-            Act (http://www.gesetze-im-internet.de /federal law/urhg/total.pdf). Downloads and copies of 
-            this page are only permitted for private and non-commercial use. If the content on our website 
-            was not created by us, the copyrights of third parties must be observed. The content of third 
-            parties is identified as such. Should you nevertheless become aware of a copyright infringement, 
-            we ask that you notify us accordingly. If we become aware of any legal violations, we will 
-            immediately remove such content.</p>
-            <p className="text-lg">This imprint was kindly provided by jurarat.de.</p>
+                <p className="text-lg">The content and works published on our website are subject to German
+                    copyright law (http://www.gesetze-im-internet.de/bundesrecht/urhg/kritik.pdf). The reproduction,
+                    processing, distribution and any kind of exploitation of the intellectual property from an
+                    intellectual and material perspective of the author outside the limits of copyright law requires
+                    the prior written consent of the respective author in accordance with the Copyright
+                    Act (http://www.gesetze-im-internet.de /federal law/urhg/total.pdf). Downloads and copies of
+                    this page are only permitted for private and non-commercial use. If the content on our website
+                    was not created by us, the copyrights of third parties must be observed. The content of third
+                    parties is identified as such. Should you nevertheless become aware of a copyright infringement,
+                    we ask that you notify us accordingly. If we become aware of any legal violations, we will
+                    immediately remove such content.</p>
+                <p className="text-lg">This imprint was kindly provided by jurarat.de.</p>
 
 
+            </div>
         </div>
 
     );
