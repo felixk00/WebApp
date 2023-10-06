4c51e185b21eda60e9344a4afa14d874422aee03
felixk00 <kienlefe@hs-pforzheim.de>
2023-10-06 22:22:28 +0200

    changed layout and added some placeholder images

--- frontend/mafilms/src/components/LandingPage.tsx
+++ frontend/mafilms/src/components/LandingPage.tsx
@@ -2,59 +2,130 @@
 
 export default function LandingPage() {
     return (
-        <div className='mx-auto flex items-center justify-center p-4'>
-            <div className="mx-auto flex max-w-4xl flex-col justify-between relative">
+        <div>
+            <div className='mx-auto flex items-center justify-center p-4'>
+                <div className="mx-auto flex max-w-4xl flex-col justify-between relative">
 
 
 
 
-                <div className="absolute lg:px-6 lg:py-4 px-4 py-2 lg:text-7xl text-2xl text-white font-bold">
-                    <div className="animate-typing1">
-                        Capture Moments
+                    <div className="absolute lg:px-6 lg:py-4 px-4 py-2 lg:text-7xl text-2xl text-white font-bold">
+                        <div className="animate-typing1">
+                            Capture Moments
+                        </div>
+                        <div className="animate-typing2">
+                            like never before.
+                        </div>
                     </div>
-                    <div className="animate-typing2">
-                        like never before.
-                    </div>
+
+
+
+
+
+
+                    <img
+                        src="/assets/pictures/DSC_0305.jpg"
+                        alt="TEstfoto"
+                        className="w-full h-auto rounded-lg"
+
+                    />
+                    {/* Desktop button */}
+                    <a
+                        id="desktopButton"
+                        className="absolute bottom-7 left-7 hidden md:inline-block 
+                    text-center px-4 py-2 text-lg font-bold w-48 border border-white 
+                    bg-white transition hover:bg-white/20 hover:text-white rounded-lg dark:text-black"
+                        href="/showroom/"
+                    >
+                        Zum Portfolio
+                    </a>
+
+                    {/* Mobile and tablet button */}
+                    <a
+                        id="mobileButton"
+                        className="absolute bottom-4 left-4 md:hidden text-center px-4 py-2 
+                    font-bold w-36 border border-white bg-white 
+                    transition hover:bg-white/10 hover:text-white rounded-lg dark:text-black"
+                        href="/showroom/"
+                    >
+                        Zum Portfolio
+                    </a>
+
+
+
+
                 </div>
-
-              
-
-
-
-
-                <img
-                    src="/assets/pictures/DSC_0305.jpg"
-                    alt="TEstfoto"
-                    className="w-full h-auto rounded-lg"
-
-                />
-                {/* Desktop button */}
-                <a
-                    id="desktopButton"
-                    className="absolute bottom-7 left-7 hidden md:inline-block text-center px-4 py-2 text-lg font-bold w-48 border border-white bg-white transition hover:bg-white/20 hover:text-white rounded-lg dark:text-black"
-                    href="/showroom/"
-                >
-                    Zum Portfolio
-                </a>
-
-                {/* Mobile and tablet button */}
-                <a
-                    id="mobileButton"
-                    className="absolute bottom-4 left-4 md:hidden text-center px-4 py-2 font-bold w-36 border border-white bg-white transition hover:bg-white/10 hover:text-white rounded-lg dark:text-black"
-                    href="/showroom/"
-                >
-                    Zum Portfolio
-                </a>
-
 
 
 
             </div>
+            <div className='mx-auto flex items-center justify-center p-4'>
+                <div className="mx-auto flex max-w-4xl flex justify-between">
+
+                    <div className="w-1/2 pr-4">
+
+                        <img
+                            src="/assets/pictures/DSC_0305.jpg"
+                            alt="TEstfoto"
+                            className="w-full h-auto rounded-lg"
+
+                        />
+
+                    </div>
+                    <div className="w-1/2 pl-4">
+                        <div>
+                            <h1 className="text-2xl font-bold">Placeholder</h1>
+
+                            <p className="text-xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                                aliquyam erat,  eirmod tempor invidunt
 
 
 
+                            </p>
+                        </div>
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
+
+            <div className='mx-auto flex items-center justify-center p-4'>
+                <div className="mx-auto flex max-w-4xl flex justify-between">
+                    <div className="w-1/2 pl-4">
+                        <div>
+                            <h1 className="text-2xl font-bold">Placeholder</h1>
+
+                            <p className="text-xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                                aliquyam erat,  eirmod tempor invidunt
+
+
+
+                            </p>
+                        </div>
+
+                    </div>
+                    <div className="w-1/2 pr-4">
+
+                        <img
+                            src="/assets/pictures/DSC_0305.jpg"
+                            alt="TEstfoto"
+                            className="w-full h-auto rounded-lg"
+
+                        />
+
+                    </div>
+
+
+                </div>
+
+
+            </div>
 
         </div>
-
     );
 } 
