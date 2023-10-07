a6b22e05acead7737b180366ddb41253629affc9
felixk00 <kienlefe@hs-pforzheim.de>
2023-10-07 21:52:18 +0200

    added some explanatory text to contact page

--- frontend/mafilms/src/app/(main)/contact/page.tsx
+++ frontend/mafilms/src/app/(main)/contact/page.tsx
@@ -13,17 +13,24 @@ export default function Contact() {
     <div className="mx-auto max-w-2xl px-4 my-10">
       <h1 className="text-5xl font-bold leading-relaxed">Contact us</h1>
 
-      <p className="text-bold text-2xl">
+      <p className="font-bold text-2xl">
         MA Films
       </p>
       <p className="text-2xl">
-        Hauptstr. 5
+        Rainweg 3
       </p>
       <p className="text-2xl">
-        76139 Karlsruhe
+        75181 Pforzheim
       </p>
       <p className="text-2xl">
         Tel. 01237654654
+      </p>
+      <br></br>
+
+
+      <p className="text-2xl font-bold">
+        Interested in a photoshooting or any other requests? Any questions? Just contact us via phone
+        or use our contact form! We are happy to hear from you.
       </p>
       <br></br>
 
