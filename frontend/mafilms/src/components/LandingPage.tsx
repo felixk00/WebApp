feef8bd755ee4db5abd33f5adaefd52532675c51
felixk00 <kienlefe@hs-pforzheim.de>
2023-10-07 21:50:14 +0200

    added content to landingpage

--- frontend/mafilms/src/components/LandingPage.tsx
+++ frontend/mafilms/src/components/LandingPage.tsx
@@ -4,7 +4,7 @@ export default function LandingPage() {
     return (
         <div>
             <div className='mx-auto flex items-center justify-center p-4'>
-                <div className="mx-auto flex max-w-4xl flex-col justify-between relative">
+                <div className="mx-auto flex max-w-7xl flex-col justify-between relative">
 
 
 
@@ -24,7 +24,7 @@ export default function LandingPage() {
 
 
                     <img
-                        src="/assets/pictures/DSC_0305.jpg"
+                        src="https://res.cloudinary.com/mafilms/image/upload/v1696680715/Projects/Liguria/DSC_2479_n8dpip.jpg"
                         alt="TEstfoto"
                         className="w-full h-auto rounded-lg"
 
@@ -38,6 +38,15 @@ export default function LandingPage() {
                         href="/showroom/"
                     >
                         Zum Portfolio
+                    </a>
+
+                    <a
+                        id="desktopButton"
+                        className="absolute bottom-7 left-60 hidden md:inline-block 
+                    text-center text-white  px-4 py-2 text-lg font-bold w-48 hover:text-white hover:underline rounded-lg dark:text-black"
+                        href="/about/"
+                    >
+                        About Us
                     </a>
 
                     {/* Mobile and tablet button */}
@@ -59,13 +68,30 @@ export default function LandingPage() {
 
 
             </div>
+
+            <div className='mx-auto p-4'>
+                <div className="mx-auto max-w-7xl">
+
+                    <h1 className="text-2xl font-bold">Welcome to MA-Films!</h1>
+                </div>
+            </div>
+            <div className='mx-auto flex items-center justify-center pr-4 pl-4 pb-4'>
+                <div className="mx-auto flex max-w-7xl flex justify-between">
+                    <p className="text-xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat,  eirmod tempor invidunt</p>
+
+                </div>
+
+            </div>
+
             <div className='mx-auto flex items-center justify-center p-4'>
-                <div className="mx-auto flex max-w-4xl flex justify-between">
+                <div className="mx-auto flex max-w-7xl flex justify-between">
 
                     <div className="w-1/2 pr-4">
 
                         <img
-                            src="/assets/pictures/DSC_0305.jpg"
+                            src="https://res.cloudinary.com/mafilms/image/upload/v1696680716/Projects/Liguria/DSC_2486_byeqmi.jpg"
                             alt="TEstfoto"
                             className="w-full h-auto rounded-lg"
 
@@ -73,46 +99,9 @@ export default function LandingPage() {
 
                     </div>
                     <div className="w-1/2 pl-4">
-                        <div>
-                            <h1 className="text-2xl font-bold">Placeholder</h1>
-
-                            <p className="text-xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                                aliquyam erat,  eirmod tempor invidunt
-
-
-
-                            </p>
-                        </div>
-
-                    </div>
-
-
-                </div>
-
-
-            </div>
-
-            <div className='mx-auto flex items-center justify-center p-4'>
-                <div className="mx-auto flex max-w-4xl flex justify-between">
-                    <div className="w-1/2 pl-4">
-                        <div>
-                            <h1 className="text-2xl font-bold">Placeholder</h1>
-
-                            <p className="text-xl">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
-                                sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
-                                aliquyam erat,  eirmod tempor invidunt
-
-
-
-                            </p>
-                        </div>
-
-                    </div>
-                    <div className="w-1/2 pr-4">
 
                         <img
-                            src="/assets/pictures/DSC_0305.jpg"
+                            src="https://res.cloudinary.com/mafilms/image/upload/v1696680723/Projects/Liguria/DSC_2506_pgjznu.jpg"
                             alt="TEstfoto"
                             className="w-full h-auto rounded-lg"
 
@@ -121,11 +110,88 @@ export default function LandingPage() {
                     </div>
 
 
+
                 </div>
 
 
             </div>
+            <div className='mx-auto p-4 flex items-center justify-center'>
+                <div className="mx-auto max-w-7xl">
+                    <hr className="border-t border-gray-300 my-4" />
 
-        </div>
+                    <h1 className="text-2xl font-bold flex items-center justify-center">Placeholder</h1>
+
+
+
+                    <p className="text-xl flex items-center justify-center">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat,  eirmod tempor invidunt</p>
+
+                    <hr className="border-t border-gray-300 my-4" />
+
+
+                </div>
+
+            </div>
+
+
+           
+
+           
+
+            <div className='mx-auto flex items-center justify-center p-4'>
+                <div className="mx-auto flex max-w-7xl flex-col justify-between relative pt-4">
+
+
+                    <img
+                        src="https://res.cloudinary.com/mafilms/image/upload/v1696680715/Projects/Liguria/DSC_2479_n8dpip.jpg"
+                        alt="TEstfoto"
+                        className="w-full h-auto rounded-lg"
+
+                    />
+                    {/* Desktop button */}
+                    <a
+                        id="desktopButton"
+                        className="absolute bottom-7 left-7 hidden md:inline-block 
+                    text-center text-white px-4 py-2 text-lg font-bold w-48 border border-white 
+                     transition hover:bg-white/20 hover:text-white rounded-lg dark:text-black"
+                        href="/contact/"
+                    >
+                        Contact Us
+                    </a>
+
+                    {/* Mobile and tablet button */}
+                    <a
+                        id="mobileButton"
+                        className="absolute bottom-4 left-4 md:hidden text-center px-4 py-2 
+                    font-bold w-36 border border-white bg-white 
+                    transition hover:bg-white/10 hover:text-white rounded-lg dark:text-black"
+                        href="/contact/"
+                    >
+                        Contact Us
+                    </a>
+                </div>
+            </div>
+
+            <div className='mx-auto p-4 flex items-center justify-center'>
+                <div className="mx-auto max-w-7xl">
+                    <hr className="border-t border-gray-300 my-4" />
+
+                    <h1 className="text-2xl font-bold flex items-center justify-center">Placeholder</h1>
+
+
+
+                    <p className="text-xl flex items-center justify-center">Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+                        sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+                        aliquyam erat,  eirmod tempor invidunt</p>
+
+                    <hr className="border-t border-gray-300 my-4" />
+
+
+                </div>
+
+            </div>
+
+        </div >
     );
 } 
