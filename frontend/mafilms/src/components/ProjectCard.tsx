7867a9ce5711b9d79110d3b49e03748586999e79
melih-avci <melih-avci@outlook.de>
2023-10-09 13:15:44 +0200

    changed background to nextjs image component

--- frontend/mafilms/src/components/ProjectCard.tsx
+++ frontend/mafilms/src/components/ProjectCard.tsx
@@ -1,5 +1,6 @@
 import Logo from '@/components/Logo';
 import Link from 'next/link';
+import Image from 'next/image';
 
 type CardProps = {
   href: string;
@@ -18,22 +19,22 @@ export default function ProjectCard({
 }: CardProps) {
   return (
     <Link href={href}>
-      <div
-        className='after:content shadow-highlight after:shadow-highlight relative mb-5 flex aspect-[2/3] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white brightness-90 transition after:pointer-events-none after:absolute after:inset-0 after:rounded-lg hover:brightness-110 lg:pt-0'
-        style={{
-          backgroundImage: backgroundImage ? `url(${backgroundImage})` : 'none',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-        }}
-      >
-        <div className='absolute inset-0 flex items-center justify-center opacity-20'>
+      <div className='after:content shadow-highlight after:shadow-highlight relative mb-5 flex aspect-[2/3] flex-col items-center justify-end gap-4 overflow-hidden rounded-lg bg-white/10 px-6 pb-16 pt-64 text-center text-white brightness-90 transition after:pointer-events-none after:absolute after:inset-0 after:rounded-lg hover:brightness-110 lg:pt-0'>
+        <Image
+          alt='A Shot by Melih Dihya'
+          className='z-0'
+          style={{ transform: 'translate3d(0, 0, 0)' }}
+          src={`${backgroundImage}`}
+          fill
+        />
+        <div className='absolute inset-0 z-20 flex items-center justify-center opacity-20'>
           <span className='absolute bottom-0 left-0 right-0 h-[400px] bg-gradient-to-b from-black/0 via-black to-black'></span>
         </div>
-        {showLogo && <Logo />}
-        <h1 className='mb-4 mt-8 text-base font-bold uppercase tracking-widest'>
+        <div className='z-30'>{showLogo && <Logo />}</div>
+        <h1 className='z-10 mb-4 mt-8 text-base font-bold uppercase tracking-widest'>
           {title}
         </h1>
-        <p className='max-w-[40ch] text-white/75 sm:max-w-[32ch]'>
+        <p className='z-10 max-w-[40ch] text-white/75 sm:max-w-[32ch]'>
           {description}
         </p>
       </div>
